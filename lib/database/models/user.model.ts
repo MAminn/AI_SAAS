e05398d8b74230628dbe3e5b09fa14fe251d8c72
muhammed Borham <muhammeddamiinn@gmail.com>
2024-05-19 15:41:58 +0300

    implement clerk webhooks

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -11,17 +11,16 @@ export interface IUser extends Document {
   creditBalance?: number;
 }
 
-
 const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   photo: { type: String, required: true },
-  firstName: { type: String},
+  firstName: { type: String },
   lastName: { type: String },
   planId: { type: Number, default: 1 },
   creditBalance: { type: Number, default: 10 },
 });
 
-const User = models.User || model('User', UserSchema);
+const User = models.User || model("User", UserSchema);
 export default User;
